65ce431c21e8ebc3abea618908dad8c2b93c9914
Andreas Gerasimow <andreas.gerasimow12@gmail.com>
2023-08-28 11:50:10 +0200

    Updated logos

--- components/FooterPart.vue
+++ components/FooterPart.vue
@@ -5,10 +5,10 @@
     </div>
     <div class="flex mt-5 lg:mt-0 gap-5">
       <a href="https://github.com/dev-andreas" target="_blank" rel="noopener">
-        <img src="~/assets/images/social_nets/github.svg" alt="github" class="w-12 flat-shadow-sm" />
+        <img src="~/assets/images/social_nets/github.svg" alt="github" class="h-12 flat-shadow-sm" />
       </a>
       <a href="https://www.linkedin.com/in/andreas-gerasimow-341a551b2/" target="_blank" rel="noopener">
-        <img src="~/assets/images/social_nets/linkedin.svg" alt="linkedin" class="w-12 flat-shadow-sm" />
+        <img src="~/assets/images/social_nets/linkedin.svg" alt="linkedin" class="h-12 flat-shadow-sm" />
       </a>
     </div>
   </footer>
