c0776622a639364e28789b304f6651cb7635b3f3
Andreas Gerasimow <andreas.gerasimow12@gmail.com>
2024-03-17 17:40:46 +0100

    graphical changes

--- components/TimeLine.vue
+++ components/TimeLine.vue
@@ -3,16 +3,18 @@
     <div class="absolute w-0 h-full border-l border-font-dark left-20 sm:left-28 -translate-x-3 sm:-translate-x-1 z-0"></div>
     <div class="absolute w-0 top-full h-6 border-l border-dashed border-font-dark left-20 sm:left-28 -translate-x-3 sm:-translate-x-1 z-0"></div>
     <span class="h-0"></span>
-    <li v-for="item, index in props.items" class="flex items-center" :key="item.title">
+    <li v-for="item, index in props.items"
+      class="flex items-center z-10" 
+      :key="item.title"
+      @mouseover="onHover(index)"
+      @mouseleave="onLeave()" ref>
       <p class="w-10 sm:w-20 sm:whitespace-nowrap transition-all duration-200 ease-out" :class="{ 'font-bold': index == hover }">{{ item.time }}</p>
-      <div class="mx-5 flex items-center z-10">
+      <div class="mx-5 flex items-center">
         <div class="w-4 h-4 rounded-full border border-font-dark transition-all duration-200 ease-out" :class="[index == hover ? 'bg-font-dark' : 'bg-backg-light']"></div>
         <div class="w-6 h-0 border-b border-font-dark"></div>
         <div class="w-2 h-2 bg-font-dark rounded-full"></div>
       </div>
-      <div class="bgg p-3 rounded-lg w-64 sm:w-96 lg:w-64 xl:w-96 pr-8" 
-        @mouseover="onHover(index)"
-        @mouseleave="onLeave()">
+      <div class="p-3 rounded-lg w-64 sm:w-96 lg:w-64 xl:w-96">
         <p><b>{{ item.title }}</b>, {{ item.institution }}</p>
         <ul>
           <li class="list-disc translate-x-5" v-for="detail in item.details">{{ detail }}</li>
